4062f0445708c6ae635f0e46e124a46df5280113
Licardo <albert.abdilim@foxmail.com>
2025-10-28 21:04:20 +0800

    UpyunConfigView

--- macOS/View/Settings/Hosts/ConfigView/UpyunConfigView.swift
+++ macOS/View/Settings/Hosts/ConfigView/UpyunConfigView.swift
@@ -6,10 +6,87 @@
 //
 
 import SwiftUI
+import UPicCore
 
 struct UpyunConfigView: View {
+    @State private var bucket: String = ""
+    @State private var operatorName: String = ""
+    @State private var password: String = ""
+    @State private var domain: String = ""
+    @State private var saveKey: String = "uPic/{filename}{.suffix}"
+    @State private var saveKeySuffix: String = ""
+    @State private var isPasswordSecured: Bool = true
+
+    @Environment(\.openURL) var openURL
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            // Bucket
+            TextField("Bucket", text: $bucket)
+                .frame(height: 30)
+
+            // Operator
+            TextField("Operator", text: $operatorName, prompt: Text("Operator name"))
+                .frame(height: 30)
+
+            // Password
+            HStack {
+                if isPasswordSecured {
+                    SecureField("Password", text: $password, prompt: Text("Operator password"))
+                } else {
+                    TextField("Password", text: $password, prompt: Text("Operator password"))
+                }
+
+                Button {
+                    isPasswordSecured.toggle()
+                } label: {
+                    Image(systemName: isPasswordSecured ? "eye.slash" : "eye")
+                        .foregroundStyle(isPasswordSecured ? .primary : Color.blue)
+                }
+                .buttonStyle(.plain)
+                .disabled(password.isEmpty)
+            }
+            .frame(height: 30)
+
+            // Domain
+            TextField("Domain", text: $domain, prompt: Text(verbatim: "https://your-domain.com"))
+                .frame(height: 30)
+
+            // Save Key Path
+            HStack {
+                TextField("Save Key", text: $saveKey)
+                    .fontDesign(.monospaced)
+                TextField("", text: $saveKeySuffix, prompt: Text("!w"))
+                    .labelsHidden()
+                    .frame(minWidth: 40)
+                    .fixedSize()
+                    .fontDesign(.monospaced)
+                    .help("The suffix added during the visit does not affect the upload(also supports variables). Can be used as object storage for image processing styles, etc ... For example: !w means get a watermarked image.")
+            }
+            .frame(height: 30)
+
+            Text("""
+            Supports {year} {month} {day} {hour} {minute} {second} {since_second} {since_millisecond} {random} {filename} {.suffix} {suffix} {mimetype} and etc. For example, the uploaded file is uPic.jpg, set to "uPic/{filename}{.suffix}", it will be saved as: uPic/uPic.jpg.
+            """)
+            .font(.caption)
+            .foregroundStyle(.secondary)
+            .frame(maxHeight: .infinity, alignment: .topLeading)
+
+            // Help Links
+            HStack {
+                Spacer()
+                Button {
+                    if let url = URL(string: "https://blog.svend.cc/upic/tutorials/upyun_uss") {
+                        openURL(url)
+                    }
+                } label: {
+                    Image(systemName: "questionmark")
+                        .padding(2)
+                }
+                .buttonBorderShape(.circle)
+            }
+        }
+        .padding()
     }
 }
 
